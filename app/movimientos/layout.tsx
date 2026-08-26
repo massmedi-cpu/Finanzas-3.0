e0cd6bce9c23737631be26cf7d34bf179e7afbea
massmedi-cpu <massmedi@gmail.com>
2026-08-26 22:00:29 +0200

    style: load reconciliation workbench styles

--- app/movimientos/layout.tsx
+++ app/movimientos/layout.tsx
@@ -1,4 +1,5 @@
 import "../movements.css";
 import "./movement-documents.css";
+import "./conciliacion/reconciliation-workbench.css";
 
 export default function Layout({children}:{children:React.ReactNode}){return children}
