c513c144bcdd5ba5ecc065122a81e5bef5976ad2
massmedi-cpu <massmedi@gmail.com>
2026-08-28 12:50:45 +0200

    style(v6.1): load matching history styles in control center

--- app/control/layout.tsx
+++ app/control/layout.tsx
@@ -1,5 +1,6 @@
 import "../control.css";
 import "./integrity.css";
 import "./matching-quality.css";
+import "./document-matching-history.css";
 
 export default function Layout({children}:{children:React.ReactNode}){return children}
