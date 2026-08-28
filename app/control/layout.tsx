878579439daabfde2d218db27320407b9ea79f67
massmedi-cpu <massmedi@gmail.com>
2026-08-28 13:01:08 +0200

    style(v6.1): load matching calibration styles

--- app/control/layout.tsx
+++ app/control/layout.tsx
@@ -2,5 +2,6 @@ import "../control.css";
 import "./integrity.css";
 import "./matching-quality.css";
 import "./document-matching-history.css";
+import "./document-matching-calibration.css";
 
 export default function Layout({children}:{children:React.ReactNode}){return children}
