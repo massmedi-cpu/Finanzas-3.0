210a0d058acb671f8e2e94e4b83c01f1444ab110
massmedi-cpu <massmedi@gmail.com>
2026-09-01 06:51:05 +0200

    Protect Rules modal focus and keyboard

--- app/reglas/layout.tsx
+++ app/reglas/layout.tsx
@@ -1,1 +1,4 @@
-import "../rules.css";export default function Layout({children}:{children:React.ReactNode}){return children}
+import { AccessibleDialogBoundary } from "@/components/accessible-dialog-boundary";
+import "../rules.css";
+
+export default function Layout({children}:{children:React.ReactNode}){return <AccessibleDialogBoundary>{children}</AccessibleDialogBoundary>}
