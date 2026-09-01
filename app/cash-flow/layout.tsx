6a19d35bda9fa2739b2fd9b369b6fdfbe10b7523
massmedi-cpu <massmedi@gmail.com>
2026-09-01 04:50:00 +0200

    Load shared Cash Flow chart styles explicitly

--- app/cash-flow/layout.tsx
+++ app/cash-flow/layout.tsx
@@ -1,4 +1,5 @@
 import "../cash-flow.css";
+import "../cash-flow-chart.css";
 import "../forecast-liquidity.css";
 
 export default function Layout({children}:{children:React.ReactNode}){return children}
