59ad78c0aad59f92e492ddd35f7b474df5404d95
massmedi-cpu <massmedi@gmail.com>
2026-09-01 06:50:57 +0200

    Protect Goals modal focus and keyboard

--- app/objetivos/layout.tsx
+++ app/objetivos/layout.tsx
@@ -1,1 +1,4 @@
-import "../goals.css";export default function Layout({children}:{children:React.ReactNode}){return children}
+import { AccessibleDialogBoundary } from "@/components/accessible-dialog-boundary";
+import "../goals.css";
+
+export default function Layout({children}:{children:React.ReactNode}){return <AccessibleDialogBoundary>{children}</AccessibleDialogBoundary>}
