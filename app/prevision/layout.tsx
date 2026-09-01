1b0616e22a35b865a4381ee5f0e71506b9c3ca46
massmedi-cpu <massmedi@gmail.com>
2026-09-01 09:54:38 +0200

    Load shared module surfaces in Forecast

--- app/prevision/layout.tsx
+++ app/prevision/layout.tsx
@@ -1,4 +1,5 @@
 import { AccessibleDialogBoundary } from "@/components/accessible-dialog-boundary";
+import "../module-surfaces.css";
 import "../editor-dialog.css";
 import "../forecast.css";
 import "../forecast-ledger.css";
