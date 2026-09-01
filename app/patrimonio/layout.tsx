c1a9af68fb81f3ca89b3be704f6b34499550423a
massmedi-cpu <massmedi@gmail.com>
2026-09-01 08:10:32 +0200

    Protect Net Worth editor with shared dialog boundary

--- app/patrimonio/layout.tsx
+++ app/patrimonio/layout.tsx
@@ -1,1 +1,5 @@
-import "../net-worth.css";export default function Layout({children}:{children:React.ReactNode}){return children}
+import { AccessibleDialogBoundary } from "@/components/accessible-dialog-boundary";
+import "../detail-dialog.css";
+import "../net-worth.css";
+
+export default function Layout({children}:{children:React.ReactNode}){return <AccessibleDialogBoundary>{children}</AccessibleDialogBoundary>}
