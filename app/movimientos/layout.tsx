20d522750e59da4e07abe8291dfbbb4009047b23
massmedi-cpu <massmedi@gmail.com>
2026-09-01 19:50:35 +0200

    style: load bulk movement operation styles

--- app/movimientos/layout.tsx
+++ app/movimientos/layout.tsx
@@ -4,6 +4,7 @@ import "../detail-dialog.css";
 import "../movements.css";
 import "../document-linking.css";
 import "./movement-documents.css";
+import "./bulk-operations.css";
 import "./conciliacion/reconciliation-workbench.css";
 
 export default function Layout({children}:{children:React.ReactNode}){return <AccessibleDialogBoundary>{children}</AccessibleDialogBoundary>}
