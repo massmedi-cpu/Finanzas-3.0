80086dd5913a6d472c23a114d84266ac94911c6a
massmedi-cpu <massmedi@gmail.com>
2026-09-07 08:22:29 +0200

    build(f11): trace Tesseract runtime assets for document OCR

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,22 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const ocrRuntimeAssets = [
+  "./node_modules/tesseract.js/**/*",
+  "./node_modules/tesseract.js-core/**/*",
+  "./node_modules/@tesseract.js-data/spa/**/*",
+  "./node_modules/regenerator-runtime/**/*",
+  "./node_modules/wasm-feature-detect/**/*",
+  "./node_modules/zlibjs/**/*",
+  "./node_modules/bmp-js/**/*",
+  "./node_modules/is-url/**/*",
+  "./node_modules/node-fetch/**/*",
+  "./node_modules/idb-keyval/**/*",
+];
+
+const nextConfig: NextConfig = {
+  outputFileTracingIncludes: {
+    "/api/documents": ocrRuntimeAssets,
+  },
+};
 
 export default nextConfig;
