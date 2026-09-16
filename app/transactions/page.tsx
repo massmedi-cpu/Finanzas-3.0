dc8acdbf3ebbc9bda807453dd6a85f8c92105fca
massmedi-cpu <massmedi@gmail.com>
2026-09-16 21:26:49 +0200

    feat(transactions): surface quick filters above movement list

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,9 +1,11 @@
 import AppShell from "../app-shell";
 import TransactionsClient from "./transactions-client";
+import TransactionsQuickNav from "./transactions-quick-nav";
 
 export default function TransactionsPage() {
   return (
     <AppShell>
+      <TransactionsQuickNav />
       <TransactionsClient />
     </AppShell>
   );
